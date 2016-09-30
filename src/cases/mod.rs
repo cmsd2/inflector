e462d8fb9c41a840bfdd6601f715949ff92b8918
Whatisinternet <joshteeter@gmail.com>
2016-09-30 11:25:09 -0400

    Removed lower and upper cases

--- src/cases/mod.rs
+++ src/cases/mod.rs
@@ -38,14 +38,4 @@ pub mod titlecase;
 /// Example string `table_cases`
 pub mod tablecase;
 
-/// Provides conversion to upper case strings.
-///
-/// Example string `UPPERCASE`
-pub mod uppercase;
-
-/// Provides conversion to lower case strings.
-///
-/// Example string `LOWERCASE`
-pub mod lowercase;
-
 mod case;
